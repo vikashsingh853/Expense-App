e5adeaec06eea6b13c34c32b7b0a8253ab7e27f0
V1singham <44531543+V1singham@users.noreply.github.com>
2022-02-06 13:07:48 +0530

    publish

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Book',
+    amount: 944,
+    date: new Date(2021, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 11799, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 2940,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
