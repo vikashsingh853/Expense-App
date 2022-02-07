3c6d3ab63e79d581c59a2a76fc0b08a2c4d14862
Vikash kumar Singh <44531543+V1singham@users.noreply.github.com>
2022-02-07 16:52:35 +0530

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -27,7 +27,7 @@ const Expenses = (props) => {
         <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
-     <h3> VKS </h3>
+     <div> VKS </div>
     </div>
   );
 };
